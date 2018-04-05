b5e131a5a8deb1c373f591b97b93f29ba728e23a
Julien <julien.alexandre26@gmail.com>
2018-04-05 23:52:32 +0200

    Importation des personnages à partir de l'API. Transformation des personnages en objet. Classement des personnages en fonction de leur alignement.

--- src/Controller/SuperController.php
+++ src/Controller/SuperController.php
@@ -8,7 +8,7 @@
  */
 
 namespace Controller;
-use GuzzleHttp\Exception\GuzzleException;
+use Model\Hero;
 
 
 /**
@@ -22,9 +22,40 @@ class SuperController extends AbstractController
      * Display home
      *
      * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function index()
     {
+        session_start();
+
+        $clientGuzzle = new \GuzzleHttp\Client([
+                'base_uri' => 'https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/',
+            ]
+        );
+
+        try {
+            $heroesList = $clientGuzzle->request('GET', 'all.json');
+        } catch (\Exception $e) {
+            $e->getMessage();
+        }
+
+        $heroes = json_decode($heroesList->getBody());
+
+        $good = [];
+        $neutral = [];
+        $bad = [];
+
+        foreach ($heroes as $hero) {
+            $alignment = $hero->biography->alignment;
+            if ($alignment == 'good') {
+                $good[] = new Hero(intval($hero->id), $hero->name, intval($hero->powerstats->intelligence), intval($hero->powerstats->strength), intval($hero->powerstats->speed), intval($hero->powerstats->durability), intval($hero->powerstats->power), intval($hero->powerstats->combat), $hero->appearance->gender, $hero->appearance->race, $hero->biography->alignment, $hero->images->sm);
+            } elseif ($alignment == 'neutral' || $alignment == '-') {
+                $neutral[] = new Hero(intval($hero->id), $hero->name, intval($hero->powerstats->intelligence), intval($hero->powerstats->strength), intval($hero->powerstats->speed), intval($hero->powerstats->durability), intval($hero->powerstats->power), intval($hero->powerstats->combat), $hero->appearance->gender, $hero->appearance->race, $hero->biography->alignment, $hero->images->sm);
+            } else {
+                $bad[] = new Hero(intval($hero->id), $hero->name, intval($hero->powerstats->intelligence), intval($hero->powerstats->strength), intval($hero->powerstats->speed), intval($hero->powerstats->durability), intval($hero->powerstats->power), intval($hero->powerstats->combat), $hero->appearance->gender, $hero->appearance->race, $hero->biography->alignment, $hero->images->sm);
+            }
+        }
+
         try {
             return $this->twig->render('Super/index.html.twig', []);
         } catch (\Exception $e) {
@@ -39,6 +70,10 @@ class SuperController extends AbstractController
      */
     public function team()
     {
+        // Récupérer 12 héros aléatoirement en fonction de l'alignement
+            // Générer 12 id de 1 à 731
+
+
         try {
             return $this->twig->render('Super/team.html.twig', []);
         } catch (\Exception $e) {
